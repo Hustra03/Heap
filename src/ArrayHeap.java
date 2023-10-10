1153e1445d0a8bb26d312d046bbad996c65705a8
Hustra03 <erik.paulinder@gmail.com>
2023-10-10 11:58:05 +0200

    modified:   bin/ArrayHeap.class 	modified:   src/ArrayHeap.java

--- src/ArrayHeap.java
+++ src/ArrayHeap.java
@@ -1,3 +1,29 @@
 public class ArrayHeap {
-    
+    int heap[];
+    int currentMaxIndex;
+
+    ArrayHeap(int length) {
+        this.heap = new int[length];
+    }
+
+    public void bubble(int value) {
+        currentMaxIndex += 1;
+        int currentPosition = currentMaxIndex + 1;
+        heap[currentPosition] = value;
+        
+        int previousPostion = (currentPosition - 2) / 2;
+        if ((currentPosition%2)== 0 ) {
+            previousPostion = (currentPosition - 2) / 2;
+        }
+        else
+        {previousPostion = (currentPosition - 1) / 2;}
+        while (heap[currentPosition] > heap[previousPostion]) {
+            int tempValue = heap[currentPosition];
+            heap[previousPostion] = heap[currentPosition];
+            heap[currentPosition] = tempValue;
+
+            currentPosition = previousPostion;
+            previousPostion = (currentPosition - 2) / 2;
+        }
+    }
 }
